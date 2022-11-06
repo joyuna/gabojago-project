17901c41346200500078895a179bc177b60792a9
yumin <cfy113@naver.com>
2022-11-06 20:31:33 +0900

    Feat: 고객지원_공지사항 등록, 조회, 상세보기 구현

--- app/src/main/java/com/bitcamp/gabojago/service/MemberService.java
+++
@@ -1,9 +0,0 @@
-package com.bitcamp.gabojago.service;
-
-import com.bitcamp.gabojago.vo.Member;
-
-
-public interface MemberService {
-
-  Member get(String id, String password) throws Exception;
-}
