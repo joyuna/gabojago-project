629315e07c4c8295fefa8de7a4f81d9e346f4a29
Dingadung <1229jimin@naver.com>
2022-11-10 21:57:01 +0900

    feat: 코스추천글 댓글 입력 기능 추가

--- app/src/main/java/com/bitcamp/gabojago/web/RecommendationController.java
+++ app/src/main/java/com/bitcamp/gabojago/web/RecommendationController.java
@@ -16,9 +16,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @Controller
@@ -132,6 +130,45 @@ public class RecommendationController {
 
     return "redirect:recommendationList";
   }
+
+  //------------------------JangComment----------------------------
+//  @Autowired
+//  JangCommentService jangCommentService;
+
+  @ResponseBody  //데이터 전달 빠르게 확인 가능
+  @RequestMapping("comment-select-list/{recono}")
+  public List<JangComment> jangCommentList(@PathVariable("recono") int recono, Model model) throws Exception{
+    return jangCommentService.jangCommentList(recono);
+  }
+
+  @PostMapping("jangCommentAdd")
+  public String jangCommentInsert(JangComment jangComment,
+                                  HttpSession session) throws Exception {
+    jangCommentService.jangCommentInsert(jangComment);
+    return "redirect:../recommendation/recommendationDetail?recono="+jangComment.getRecono();
+  }
+
+  @GetMapping("jangCommentDelete")
+  public String jangCommentDelete(int cmno, HttpSession session, ExhibitionReview exhibitionReview) throws Exception {
+    //  checkOwner(no, session);
+    if(!jangCommentService.jangCommentDelete(cmno)) {
+      throw new Exception("댓글을 삭제 할 수 없습니다.");
+    }
+
+    return "redirect:../recommendation/recommendationList";
+  }
+
+  @PostMapping("update")
+  public String update(JangComment jangComment, HttpSession session) throws Exception{
+
+//  checkOwner(board.getNo(), session);
+
+    if(!jangCommentService.jangCommentUpdate(jangComment)){
+      throw new Exception("댓글을 변경 할 수 없습니다!");
+    }
+    return "redirect:../recommendation/recommendationList";
+  }
+
 
 }
 
