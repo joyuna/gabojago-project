7a294c4284069650286103dd5aefb630b0512e8e
jongHyeon0000 <jongHyeon0000>
2022-11-16 23:32:32 +0900

    feat :  - ShowCartDao : CartDao 로 클래스 이름 변경  - ShowCart.html : 코드 정렬 add :  - CartDao : 장바구니 조회 쿼리 작성

---
+++ app/src/main/java/com/bitcamp/gabojago/dao/cart/CartDao.java
@@ -0,0 +1,13 @@
+package com.bitcamp.gabojago.dao.cart;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface CartDao {
+
+  List<Map<String, String>> showCart(String id);
+	
+}
